1c3a401f31bbbd2970f1c550367f7a063640ba82
Tri Waluyo <46350497+triwaluyo07@users.noreply.github.com>
2022-06-16 16:04:32 +0700

    AboutActivity

--- app/src/main/java/com/example/kamusindonesia_jepang/activities/MainActivity.java
+++ app/src/main/java/com/example/kamusindonesia_jepang/activities/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.kamusindonesia_jepang.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.kamusindonesia_jepang.R;
 import com.example.kamusindonesia_jepang.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity
@@ -32,5 +38,22 @@ public class MainActivity extends AppCompatActivity
             Intent i = new Intent(this, HurufJepangActivity.class);
             startActivity(i);
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.nav_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.nav_about)
+        {
+            Toast.makeText(this, "about me", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
     }
 }
