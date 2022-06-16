01750fd30c6b810e0c11e806b136d168bd66926d
Deviko Kelvin <deviko.evangelion@gmail.com>
2022-06-16 16:45:06 +0700

    About Activity updates
    > Adding About page to the apps on the menu options.

--- app/src/main/java/com/example/kamusindonesia_jepang/activities/AboutActivity.kt
+++ app/src/main/java/com/example/kamusindonesia_jepang/activities/AboutActivity.kt
@@ -2,14 +2,18 @@ package com.example.kamusindonesia_jepang.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kamusindonesia_jepang.R
 import com.example.kamusindonesia_jepang.databinding.ActivityAboutBinding
 
-class AboutActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityAboutBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
+class AboutActivity : AppCompatActivity()
+{
+    private lateinit var binding : ActivityAboutBinding
+    override fun onCreate(savedInstanceState : Bundle?)
+    {
         super.onCreate(savedInstanceState)
         binding = ActivityAboutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.title = getString(R.string.about);
     }
 }
--- app/src/main/java/com/example/kamusindonesia_jepang/activities/MainActivity.java
+++ app/src/main/java/com/example/kamusindonesia_jepang/activities/MainActivity.java
@@ -2,14 +2,12 @@ package com.example.kamusindonesia_jepang.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
-
 import com.example.kamusindonesia_jepang.R;
 import com.example.kamusindonesia_jepang.databinding.ActivityMainBinding;
 
@@ -24,34 +22,40 @@ public class MainActivity extends AppCompatActivity
         bind = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(bind.getRoot());
 
-        bind.btnIndJpn.setOnClickListener(view -> {
+        bind.btnIndJpn.setOnClickListener(view ->
+        {
             Intent i = new Intent(this, IndToJpnActivity.class);
             startActivity(i);
         });
 
-        bind.btnJpnInd.setOnClickListener(view -> {
+        bind.btnJpnInd.setOnClickListener(view ->
+        {
             Intent i = new Intent(this, JpnToIndActivity.class);
             startActivity(i);
         });
 
-        bind.btnHurufJepang.setOnClickListener(view -> {
+        bind.btnHurufJepang.setOnClickListener(view ->
+        {
             Intent i = new Intent(this, HurufJepangActivity.class);
             startActivity(i);
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.nav_menu, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId()==R.id.nav_about)
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        if (item.getItemId() == R.id.nav_about)
         {
-            Toast.makeText(this, "about me", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this, AboutActivity.class);
+            startActivity(i);
             return true;
         }
         return false;
